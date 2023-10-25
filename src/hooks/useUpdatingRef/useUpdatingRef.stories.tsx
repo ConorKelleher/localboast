36a22c36baef18b8cd4779d142a772e060b41a08
Conor Kelleher <conor95kelleher@hotmail.com>
2023-10-25 14:33:42 +0100

    Add prettier

--- src/hooks/useUpdatingRef/useUpdatingRef.stories.tsx
+++ src/hooks/useUpdatingRef/useUpdatingRef.stories.tsx
@@ -1,13 +1,13 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import useUpdatingRef from '.';
-import { Text } from '@mantine/core';
-import React, { useEffect, useLayoutEffect, useRef } from 'react';
+import type { Meta, StoryObj } from "@storybook/react"
+import useUpdatingRef from "."
+import { Text } from "@mantine/core"
+import React, { useEffect, useRef } from "react"
 
 interface UseUpdatingRefDemoProps {
-  value: unknown;
+  value: unknown
 }
 interface MemoizedRefComponentProps {
-  refObj: React.MutableRefObject<string | undefined>;
+  refObj: React.MutableRefObject<string | undefined>
 }
 
 const RefComponent: React.FC<MemoizedRefComponentProps> = ({ refObj }) => {
@@ -17,9 +17,7 @@ const RefComponent: React.FC<MemoizedRefComponentProps> = ({ refObj }) => {
     refObjCountRef.current++
   }, [refObj])
 
-  return (
-    <Text>Distinct ref objects:{refObjCountRef.current}</Text>
-  )
+  return <Text>Distinct ref objects:{refObjCountRef.current}</Text>
 }
 
 const MemoizedRefComponent = React.memo(RefComponent)
@@ -27,25 +25,28 @@ const MemoizedRefComponent = React.memo(RefComponent)
 const UseUpdatingRefDemo = ({ value }: UseUpdatingRefDemoProps) => {
   const updatingRef = useUpdatingRef<string>(value as string)
   return (
-  <>
-    <Text size="xl">props.value === "{value as "string"}"</Text>
-    <Text size="xl">ref.current === "{updatingRef.current}" (ref.current updated in effect, so won't have updated at render time)</Text>
-    <MemoizedRefComponent refObj={updatingRef} />
-  </>
+    <>
+      <Text size="xl">props.value === "{value as "string"}"</Text>
+      <Text size="xl">
+        ref.current === "{updatingRef.current}" (ref.current updated in effect,
+        so won't have updated at render time)
+      </Text>
+      <MemoizedRefComponent refObj={updatingRef} />
+    </>
   )
 }
 
 const meta = {
   title: "Hooks/useUpdatingRef",
   component: UseUpdatingRefDemo,
-  tags: ['autodocs'],
-} satisfies Meta<typeof UseUpdatingRefDemo>;
+  tags: ["autodocs"],
+} satisfies Meta<typeof UseUpdatingRefDemo>
 
-export default meta;
-type Story = StoryObj<typeof UseUpdatingRefDemo>;
+export default meta
+type Story = StoryObj<typeof UseUpdatingRefDemo>
 
 export const Primary: Story = {
   args: {
     value: "Initial Value",
   },
-};
+}
